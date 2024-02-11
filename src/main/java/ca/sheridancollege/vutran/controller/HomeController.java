d7cfe11f8fdf34e685aa0898be53949f65551a97
ngan <vutran@sheridancollege.ca>
2024-02-11 14:28:10 -0500

    Update Feature is almost done. Streaming Service update for podcast left

--- src/main/java/ca/sheridancollege/vutran/controller/HomeController.java
+++ src/main/java/ca/sheridancollege/vutran/controller/HomeController.java
@@ -2,6 +2,7 @@ package ca.sheridancollege.vutran.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -62,6 +63,52 @@ public class HomeController {
 		}
 		stOfRemovedPodcast.setPodcastList(updatedStPodcastListOfRemovedPodcast);
 		pr.deleteById(id);
+		
+		model.addAttribute("podcast", new Podcast());
+		model.addAttribute("streamingService", new StreamingService());
+		model.addAttribute("podcastList", pr.findAll());
+		model.addAttribute("streamingServiceList", sr.findAll());
+		return "index";
+	}
+	
+	@GetMapping("/updateStreamingService/{id}")
+	public String updateSt(Model model, @PathVariable("id") Long id) {
+		model.addAttribute("streamingService", new StreamingService());
+		model.addAttribute("st", sr.findById(id));
+		return "updateSt";
+	}
+	
+	@PostMapping("/updateStreamingService") 
+	public String updateStFormSubmission(Model model, @ModelAttribute StreamingService st) {
+		Optional<StreamingService> mySt = sr.findById(st.getId());
+		StreamingService updatedSt = mySt.get();
+		updatedSt.setName(st.getName());
+		updatedSt.setLink(st.getLink());
+		sr.save(updatedSt);
+		
+		model.addAttribute("podcast", new Podcast());
+		model.addAttribute("streamingService", new StreamingService());
+		model.addAttribute("podcastList", pr.findAll());
+		model.addAttribute("streamingServiceList", sr.findAll());
+		return "index";
+	}
+	
+	@GetMapping("/updatePodcast/{id}")
+	public String updatePodcast(Model model, @PathVariable("id") Long id) {
+		model.addAttribute("podcast", pr.findById(id));
+		model.addAttribute("streamingServiceList", sr.findAll());
+		return "updatePodcast";
+	}
+	
+	@PostMapping("/updatePodcast") 
+	public String updatePodcastFormSubmission(Model model, @ModelAttribute Podcast podcast) {
+		Optional<Podcast> myPodcast = pr.findById(podcast.getId());
+		Podcast updatedPodcast = myPodcast.get();
+		updatedPodcast.setLink(podcast.getLink());
+		updatedPodcast.setReleaseyear(podcast.getReleaseyear());
+		updatedPodcast.setStarRating(podcast.getStarRating());
+		updatedPodcast.setTitle(podcast.getTitle());
+		pr.save(updatedPodcast);
 		
 		model.addAttribute("podcast", new Podcast());
 		model.addAttribute("streamingService", new StreamingService());
